d109e00f92da54e9fef4e38c132f565c6e68d351
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-08-24 22:38:59 +1200

    add assets from codepen

--- Pulse/App_Start/BundleConfig.cs
+++ Pulse/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Pulse
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/animatedkev.css"));
         }
     }
 }
